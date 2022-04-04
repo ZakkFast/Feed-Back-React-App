4a28d16e4b858536eaf3fe8b515ab0c5b6cfda75
Zakk Fast <zakkfastpro@gmail.com>
2022-04-04 15:45:25 -0700

    set up proptypes and conditional styling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,7 @@
-
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function feedbackList({feedback}) {
+function FeedbackList({feedback}) {
     if( !feedback || feedback.length === 0) {
         return <p>No Feedback Yet..</p>
     }
@@ -15,4 +15,14 @@ function feedbackList({feedback}) {
     )
 }
 
-export default feedbackList
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    )
+}
+
+export default FeedbackList
