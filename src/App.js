1527987eb1ff7bb81242b75ad2099d824a600fae
Zakk Fast <zakkfastpro@gmail.com>
2022-04-05 13:41:16 -0700

    added about page placeholder and routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete this?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+    if (window.confirm('Are you sure you want to delete this?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackStats feedback={feedback} />
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackList feedback={feedback}
-        handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import Card from '../components/shared/Card';
+import { Link } from 'react-router-dom';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>
+          This is a React Act to leave feedback for a product or service
+          provided.
+        </p>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to="/">Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
